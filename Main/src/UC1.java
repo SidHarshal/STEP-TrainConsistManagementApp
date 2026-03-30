469c05368c2a23dadfef5250925cb8cf107f0a08
SidHarshal <sidhanaharshal@gmail.com>
2026-03-30 08:59:39 +0530

    Add UC1 full feature

--- Main/src/UC1.java
+++ Main/src/UC1.java
@@ -7,5 +7,8 @@ void main(String[] args) {
 
     List<String> trainConsist = new ArrayList<>();
 
-    System.out.println("--Train successfully initialized....");
+    System.out.println("Train successfully initialized....");
+    System.out.println("Initial Bogie Count : " + trainConsist.size());
+    System.out.println("Current Train Consist : " + trainConsist);
+
 }
