47a6259b2ba75fde3aa9b51fef9dedf9215fa95c
SidHarshal <sidhanaharshal@gmail.com>
2026-03-30 08:55:09 +0530

    Add UC1 feature

---
+++ Main/src/UC1.java
@@ -0,0 +1,11 @@
+
+void main() {
+
+    System.out.println("====================================");
+    System.out.println("    Train Consist Management App");
+    System.out.println("====================================");
+
+    List<String> trainConsist = new ArrayList<>();
+
+    System.out.println("--Train successfully initialized....");
+}
